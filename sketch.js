d6c7f2fc930a4562f08a3f70d0eff05c580ccfb5
Cody <cody.tian@icloud.com>
2022-03-31 13:11:35 -0700

    finish edges(), map hues to home x position

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ let font
 let instructions
 
 
+let vehicles = []
+
+
 function preload() {
     font = loadFont('data/consola.ttf')
 }
@@ -24,13 +27,46 @@ function setup() {
     instructions.html(`<pre>
         [1,2,3,4,5] → no function
         z → freeze sketch</pre>`)
+
+    createCanvas(600, 300);
+    colorMode(HSB, 360, 100, 100, 100);
+    fill(210, 50, 100)
+    stroke(210, 50, 100)
+    let points = font.textToPoints('Train', 10, height/2, 130,
+                                    {sampleFactor: 0.3})
+    for (let i = 0; i < points.length; i++){
+        vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 0, width, 0, 360), 100, 100)))
+    }
+}
+
+
+function addHappyBirthdayLiya() {
+    let v = []
+
+    let points = font.textToPoints('Happy birthday Liya!', 10, height/2, 30,
+        {sampleFactor: 0.3})
+    let secondLine = font.textToPoints('2:22pm 2022.02.02', 10, 3*height/5, 30,
+        {sampleFactor: 0.3})
+    for (let i = 0; i < points.length; i++){
+        vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 0, width, 0, 360), 100, 100)))
+    } for (let i = 0; i < secondLine.length; i++){
+        vehicles.push(new Vehicle(secondLine[i].x, secondLine[i].y, color(map(secondLine[i].x, 0, width, 0, 360), 100, 100)))
+    }
+
+    return v
 }
 
 
 function draw() {
     background(234, 34, 24)
 
-
+    for (let i = 0; i < vehicles.length; i++){
+        let vh = vehicles[i]
+        vh.show()
+        vh.update()
+        vh.behaviors()
+        vh.edges()
+    }
 
     displayDebugCorner()
 }
@@ -43,7 +79,7 @@ function displayDebugCorner() {
     const LINE_SPACING = 2
     const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
     fill(0, 0, 100, 100) /* white */
-    strokeWeight(0)
+    noStroke()
 
     text(`frameCount: ${frameCount}`,
         LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
---
+++ vehicle.js
@@ -0,0 +1,108 @@
+function Vehicle(x, y, c){
+    this.pos = new p5.Vector(random(width), random(height))
+    this.vel = p5.Vector.random2D()
+    this.acc = new p5.Vector(0, 0)
+    this.r = 1
+    this.target = new p5.Vector(x, y)
+    this.maxSpeed = 4
+    this.maxForce = 0.3
+    this.c = c
+}
+
+
+// shows the vehicle
+Vehicle.prototype.show = function(){
+    fill(this.c)
+    stroke(this.c)
+    strokeWeight(3)
+    circle(this.pos.x, this.pos.y, this.r*2)
+}
+
+// updates the vehicle's position, velocity, and acceleration
+Vehicle.prototype.update = function(){
+    this.pos.add(this.vel)
+    this.vel.add(this.acc)
+    this.acc.mult(0)
+}
+
+//applies a force to the vehicle
+Vehicle.prototype.applyForce = function(f){
+    // f = ma, and m = 1 so a = f
+    this.acc.add(f)
+}
+
+// applies the vehicle's behaviors
+Vehicle.prototype.behaviors = function(){
+    // we always want to seek our target
+    let seek = this.arrive(this.target)
+    // "I'm afraid of the mouse!", says this.
+    let mouse = new p5.Vector(mouseX, mouseY)
+    let flee = this.flee(mouse)
+    // scaling room {
+    seek.mult(1)
+    flee.mult(2)
+    //              }
+
+    // apply our forces
+    this.applyForce(seek)
+    if (mouseIsPressed){
+        this.applyForce(flee)
+    }
+}
+
+// seeks the target
+Vehicle.prototype.seek = function(target){
+    // get a line from us to the target, the first step of getting the
+    // desired velocity
+    let desired = p5.Vector.sub(target, this.pos)
+    // we always want to go at our maximum speed
+    desired.setMag(this.maxSpeed)
+    // steering = desired - current
+    desired.sub(this.vel)
+    // make sure we don't apply too much force
+    return desired.limit(this.maxForce)
+}
+
+// flees the target
+Vehicle.prototype.flee = function(target){
+    // fleeing is the opposite of seeking
+    if (p5.Vector.dist(this.pos, target) < 40) {
+        return this.seek(target).mult(-1)
+    }
+    else{
+        return new p5.Vector(0, 0)
+    }
+}
+
+// arrives at the target
+Vehicle.prototype.arrive = function(target){
+    // get a line from us to the target, the first step of getting the
+    // desired velocity
+    let desired = p5.Vector.sub(target, this.pos)
+    // we always want to go at our maximum speed, except for if we're
+    // inside a perception radius
+    let speed = this.maxSpeed
+    if (desired.mag() < 50){
+        speed = map(desired.mag(), 0, 50, 0, this.maxSpeed)
+    }
+    desired.setMag(speed)
+    // steering = desired - current
+    desired.sub(this.vel)
+    // make sure we don't apply too much force
+    return desired.limit(this.maxForce)
+}
+
+// wraps around the edges
+Vehicle.prototype.edges = function(){
+    if (this.pos.x - this.r < 0) {
+        this.pos.x = width - this.r
+    } if (this.pos.x + this.r > width) {
+        this.pos.x = this.r
+    }
+
+    if (this.pos.y + this.r > height) {
+        this.pos.y = this.r
+    } if (this.pos.y - this.r < 0) {
+        this.pos.y = height - this.r
+    }
+}
