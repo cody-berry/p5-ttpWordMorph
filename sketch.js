254d3ed91317d980fabf296395f2dd3e02294762
Cody <cody.tian@icloud.com>
2022-04-01 14:19:39 -0700

    move through scenes discontinuously, add textToPoints() functions

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@
  *
  *
  */
-let font
+let consolas
+let bpdots
 let instructions
 
 
@@ -12,7 +13,8 @@ let vehicles = []
 
 
 function preload() {
-    font = loadFont('data/consola.ttf')
+    consolas = loadFont('data/consola.ttf')
+    bpdots = loadFont('data/bpdots.otf')
 }
 
 
@@ -20,19 +22,18 @@ function setup() {
     let cnv = createCanvas(600, 300)
     cnv.parent('#canvas')
     colorMode(HSB, 360, 100, 100, 100)
-    textFont(font, 14)
 
     /* initialize instruction div */
     instructions = select('#ins')
     instructions.html(`<pre>
-        [1,2,3,4,5] → no function
+        [1,2,3] → switch scenes
         z → freeze sketch</pre>`)
 
     createCanvas(600, 300);
     colorMode(HSB, 360, 100, 100, 100);
     fill(210, 50, 100)
     stroke(210, 50, 100)
-    let points = font.textToPoints('Train', 10, height/2, 130,
+    let points = consolas.textToPoints('Start! Press 1, 2, or 3', 10, height/2, 40,
                                     {sampleFactor: 0.3})
     for (let i = 0; i < points.length; i++){
         vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 0, width, 0, 360), 100, 100)))
@@ -40,20 +41,42 @@ function setup() {
 }
 
 
-function addHappyBirthdayLiya() {
-    let v = []
+/** returns text point locations for "happy twosday! 2.22.22 2:22pm", centered
+ *  313 points
+ */
+function addTwosDay() {
+    let pts = bpdots.textToPoints('happy twosday!', 100, 100, 48, {
+        sampleFactor: 0.01, // increase for more points
+        // simplifyThreshold: 0 // increase to remove collinear points
+    })
 
-    let points = font.textToPoints('Happy birthday Liya!', 10, height/2, 30,
-        {sampleFactor: 0.3})
-    let secondLine = font.textToPoints('2:22pm 2022.02.02', 10, 3*height/5, 30,
-        {sampleFactor: 0.3})
-    for (let i = 0; i < points.length; i++){
-        vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 0, width, 0, 360), 100, 100)))
-    } for (let i = 0; i < secondLine.length; i++){
-        vehicles.push(new Vehicle(secondLine[i].x, secondLine[i].y, color(map(secondLine[i].x, 0, width, 0, 360), 100, 100)))
-    }
+    pts = pts.concat(bpdots.textToPoints('2.22.22 2:22pm', 90, 175, 48, {
+        sampleFactor: 0.06, // increase for more points
+    }))
 
-    return v
+    return pts
+}
+
+
+/** returns text point locations for "Liya", centered
+ *  313 points
+ */
+function addBigLiya() {
+    return consolas.textToPoints('Liya', 50, 200, 224, {
+        sampleFactor: 0.2, // increase for more points
+        // simplifyThreshold: 0 // increase to remove collinear points
+    })
+}
+
+
+/** returns text point locations for "happy birthday, Liya!" centered
+ *  237 points
+ */
+function addGiantTwo() {
+    return consolas.textToPoints('2', 200, 280, 384, {
+        sampleFactor: 0.2, // increase for more points
+        // simplifyThreshold: 0 // increase to remove collinear points
+    })
 }
 
 
@@ -94,5 +117,25 @@ function keyPressed() {
         noLoop()
         instructions.html(`<pre>
             sketch stopped</pre>`)
+    }
+    /* switch scenes */
+    if (key === '1') {
+        let points = addBigLiya()
+        vehicles = []
+        for (let i = 0; i < points.length; i++){
+            vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 55, width-45, 0, 360), 100, 100)))
+        }
+    } if (key === '2') {
+        let points = addGiantTwo()
+        vehicles = []
+        for (let i = 0; i < points.length; i++){
+            vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, width/2 - 80, width/2 + 100, 0, 360), 100, 100)))
+        }
+    } if (key === '3') {
+        let points = addTwosDay()
+        vehicles = []
+        for (let i = 0; i < points.length; i++){
+            vehicles.push(new Vehicle(points[i].x, points[i].y, color(map(points[i].x, 100, width-100, 0, 360), 100, 100)))
+        }
     }
 }
